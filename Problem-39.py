ac72d4461bf84acc657939a19b184cf7ee54790d
Ali Raza <114991304+Ali-Raza-12@users.noreply.github.com>
2024-09-27 16:01:17 +0500

    Convert Celsius to Fahrenheit

---
+++ Problem-39.py
@@ -0,0 +1,17 @@
+# Python Program to Convert Celsius to Fahrenheit
+
+celcius = int(input('Enter temperature in celcius:'))
+
+ferenhiet = (celcius * 9/5) + 32
+
+print(ferenhiet)
+
+
+# =========================================
+# Python Program to Convert  Fahrenheit to Celsius
+
+# farenhiet = int(input('Enter temperature in farenhiet:'))
+
+# celcius = (farenhiet - 32) - 9/5
+
+# print(celcius)
